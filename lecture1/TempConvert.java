ec354d32292d9932e95fbba6bf02af52c0580ce2
Katie Keith <kakeith406@gmail.com>
2024-02-05 11:41:36 -0500

    add lecture 1 code

---
+++ lecture1/TempConvert.java
@@ -0,0 +1,16 @@
+/*
+* Convert Fahrenheight temperature to Celsius
+*/
+public class TempConvert{
+
+	// The number we will convert 
+	public static final double FAHR = 40.0; // A constant in Java  
+
+	public static void main(String[] args){
+		double cel; // initalize 
+
+		cel = (FAHR - 32) * 5.0/9.0; 
+		System.out.println("The temp (in C) ="+cel);
+
+	}
+}
